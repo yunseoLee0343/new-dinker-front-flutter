b444c23ba59f02570d5debe1dd36b86a42ed17f6
Yunseo Lee <lys139011@gmail.com>
2024-04-10 18:37:12 +0900

    chore : Complete auth

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:new_dinker/app/app.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = const AppBlocObserver();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final authenticationRepository = AuthenticationRepository();
   await authenticationRepository.user.first;
