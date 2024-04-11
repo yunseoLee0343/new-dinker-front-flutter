c6c4c32194d8b9fb5220dd7817451d871fbfcced
Yunseo Lee <lys139011@gmail.com>
2024-04-11 11:30:16 +0900

    feat : Add fetch home ui

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_dinker/app/app.dart';
@@ -10,32 +11,128 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    final user = context.select((AppBloc bloc) => bloc.state.user);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: <Widget>[
-          IconButton(
-            key: const Key('homePage_logout_iconButton'),
-            icon: const Icon(Icons.exit_to_app),
-            onPressed: () {
-              context.read<AppBloc>().add(const AppLogoutRequested());
-            },
-          ),
-        ],
-      ),
-      body: Align(
-        alignment: const Alignment(0, -1 / 3),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Avatar(photo: user.photo),
-            const SizedBox(height: 4),
-            Text(user.email ?? '', style: textTheme.titleLarge),
-            const SizedBox(height: 4),
-            Text(user.name ?? '', style: textTheme.headlineSmall),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Image.asset('assets/logo.png', width: MediaQuery.sizeOf(context).width/3,),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.exit_to_app),
+              onPressed: () {
+                context.read<AppBloc>().add(const AppLogoutRequested());
+              },
+            ),
           ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.store),
+              label: 'Brands',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          selectedItemColor: Colors.amber[800],
+          onTap: (int index) {
+            switch(index){
+              case 0:
+                Navigator.pushNamed(context, '/home');
+                break;
+              case 1:
+                Navigator.pushNamed(context, '/brands');
+                break;
+              case 2:
+                Navigator.pushNamed(context, '/profile');
+                break;
+            }
+          },
+        ),
+
+        body: SafeArea(
+          child: CustomScrollView(
+            slivers: <Widget>[
+
+              //MiniSearchBar(),
+
+              SliverPadding(
+                padding: EdgeInsets.all(10),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      Text('New Events', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      SizedBox(height: MediaQuery.sizeOf(context).height/50,),
+                      CarouselSlider(
+                        options: CarouselOptions(
+                          height: MediaQuery.of(context).size.height/5,
+                          aspectRatio: 16 / 9,
+                          viewportFraction: 0.8,
+                          autoPlay: true,
+                          enlargeCenterPage: true,
+                        ),
+                        items: [1,2,3,4,5].map((i) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.amber,
+                                  ),
+                                  child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                              );
+                            },
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SliverPadding(
+                padding: EdgeInsets.all(10),
+                sliver: SliverToBoxAdapter(
+                    key: UniqueKey(),
+                    child: SizedBox(height: MediaQuery.sizeOf(context).height/30,)
+                ),
+              ),
+
+              SliverPadding(
+                padding: EdgeInsets.all(10),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      Text('New Products', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      SizedBox(height: MediaQuery.sizeOf(context).height/50,),
+                      Container(
+                        key: UniqueKey(),
+                        height: MediaQuery.sizeOf(context).height,
+                        child: ListView.builder(
+                          //scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              width: 100.0,
+                              child: Card(
+                                child: Text('data'),
+                              ),
+                            );
+                          }),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
